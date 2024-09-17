0aa265c4131497bff86840a217c7286a63c32f4b
Luckyone <Luckyone.Development@pm.me>
2024-09-17 23:27:12 +0200

    Revert "Texture DocFlatKek"
    This reverts commit 1a97ff355a51e4d9bf02a72abea5684ca9b43ef6.

--- ElvUI_DoctorioUI/Media/SharedMedia.lua
+++
@@ -1,6 +0,0 @@
-local LSM = LibStub('LibSharedMedia-3.0')
-
-if LSM == nil then return end
-
--- Statusbar Textures
-LSM:Register('statusbar','DocFlatKek', [[Interface\AddOns\ElvUI_DoctorioUI\Media\Textures\DocFlatKek.blp]])
