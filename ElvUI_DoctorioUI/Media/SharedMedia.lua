1a97ff355a51e4d9bf02a72abea5684ca9b43ef6
Luckyone <Luckyone.Development@pm.me>
2024-09-17 23:23:39 +0200

    Texture DocFlatKek

---
+++ ElvUI_DoctorioUI/Media/SharedMedia.lua
@@ -0,0 +1,6 @@
+local LSM = LibStub('LibSharedMedia-3.0')
+
+if LSM == nil then return end
+
+-- Statusbar Textures
+LSM:Register('statusbar','DocFlatKek', [[Interface\AddOns\ElvUI_DoctorioUI\Media\Textures\DocFlatKek.blp]])
